1285e3362e373a8cc3c35dcba98e0e74e2e65ada
Julian Vega <ingjulianvega@gmail.com>
2021-05-14 22:01:39 -0500

    :zap: Creation.Closes #1 Delete Help.md.Closes #2 .gitignore.Closes #3 application.properties.Closes #4

---
+++ src/main/java/ingjulianvega/ximic/msscasugateway/MsscAsuGatewayApplication.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscAsuGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscAsuGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingjulianvega/ximic/msscasugateway/MsscAsuGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ingjulianvega.ximic.msscasugateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscAsuGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
